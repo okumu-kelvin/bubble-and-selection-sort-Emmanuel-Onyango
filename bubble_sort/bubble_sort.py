f5a9268a890300cf4b6b457a85a525f581a87155
Emmanuel Onyango <emmanuelonyango238@gmail.com>
2025-06-20 10:10:58 +0300

    Initial Commit

--- bubble_sort/bubble_sort.py
+++ bubble_sort/bubble_sort.py
@@ -1,3 +1,12 @@
-def bubble_sort(unsorted_list):
+myarray=[13,34,66,33,12,9]
+def bubble_sort(myarray):
+    a=len(myarray)
+    for i in range(a-1):
+        for j in range(a-i-1):
+            if myarray[j]>myarray[j+1]:
+                temp=myarray[j]
+                myarray[j]=myarray[j+1]
+                myarray[j+1]=temp
     # TODO: Implement bubble sort
-    pass
+    return myarray
+print("Sorted array is: ",bubble_sort(myarray))
